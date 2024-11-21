29e2cc011fb5bda079af0ee8bd5a8cdd97dccece
Kristawan Jaroenrad <kristawanja@gmail.com>
2024-11-21 11:14:21 +0700

    Added basic pages and requirements.txt

--- webpage/urls.py
+++ webpage/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("investor", views.investor, name="investor"),
+    path("statistics", views.statistics, name="statistics")
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
 
+
+def investor(request):
+    return render(request, 'investor.html')
+
+
+def statistics(request):
+    return render(request, 'statistics.html')
