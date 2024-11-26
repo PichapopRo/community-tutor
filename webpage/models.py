cdac1226abc074aee53720a6f20c979d506d7857
Pichapop Roddauyboon <pichapop.ro@ku.th>
2024-11-26 15:28:09 +0700

    Adding date of birth field and adding migration file that automatically add category when migrate

--- webpage/models.py
+++ webpage/models.py
@@ -19,6 +19,7 @@ class UserInfo(models.Model):
     user_id = models.AutoField(primary_key=True)
     phone_number = models.CharField(max_length=20, unique=True)
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(default=datetime.now)
 
     def __str__(self):
         return f"{self.user_id} - {self.phone_number}"
